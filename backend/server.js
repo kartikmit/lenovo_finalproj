ae5657c864ee877fa33acdbfcccef804113a1dde
kartikmit <kartikmittal2063@gmail.com>
2026-07-09 17:27:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: 'https://lenovo-finalproj.vercel.app', 
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
